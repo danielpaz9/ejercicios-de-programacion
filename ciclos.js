63bba4dccda6402782387c43110c9f03aa5dc113
danielpaz9 <161846897+danielpaz9@users.noreply.github.com>
2024-03-08 09:39:09 -0500

    trabajo con el ciclo for

---
+++ ciclos.js
@@ -0,0 +1,60 @@
+// Repetir código secuencialmente
+// Iterar n cantidad de veces
+
+//Mostrar los númeors del 1 al 20
+//Ciclo For
+
+//Donde empezamos y hasta donde queremos
+
+//Variable de control incrementa
+/*
+
+for(let i 1; 1 <= 20; i++); {
+console.log(i);
+}
+*/
+
+// Do while
+//Ejecut ael código una vez hasta que se cumpla
+
+/*
+let numero;
+do {
+numero prompt("Ingresa un número mayor que 10");
+}
+while (numero <=10); */
+
+//Como mostrar un arreglo usando un ciclo
+
+/*
+let hombres = ["Felipe", "Danilo", "Fabian", "David", "Alejandro"];
+
+for(let 1 0; i <hombres.length; i++);
+console.log(hombres[i]);
+*/
+
+
+//Tabla de multiplicar 2
+
+
+/*
+let numerodetabla = parseInt(prompt(´Ingresa el numero a tabla´))
+for(let i = o; 1 <= 9; 1++) {
+    console.log(numerodetabla, " x ", i , " = ", numerodetabla*i )
+}
+
+*/
+
+
+// 1. Mostrar los numeros desde el 1 hasta el 50
+
+
+for(let i = 1; i <=50, i++) {
+console.log(i);
+}
+
+// Mostrar los numeros pares desde el 0 hasta el 20
+
+for(let i = 0; i <= 10; i++){
+console.log(i * 2);
+}
